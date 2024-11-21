9c1d68097be005c206ce375555fb80d957b5f0e0
Lucas Tameirao <lucas.atmartins@gmail.com>
2024-11-21 11:30:37 -0300

    Site Marry-Us criação de novos arquivos e atualizações de design

---
+++ html/functionPage.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        html, body{
+            margin: 0;
+            padding: 0;
+            height: 100%;
+        }
+
+        
+    </style>
+    <title>Marry-Us</title>
+</head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+    <header class="bg-primary border-bottom border-2 border-dark">
+      <div class="d-flex justify-content-between py-2 px-3 bg-light">
+        <h1 class="h3 mb-0">Marry-Us</h1>
+        <nav>
+
+        </nav>
+      </div>
+    </header>
+
+    <Main>
+        <div class="d-flex min-vh-100 flex-grow-1">
+            <div class="bg-light d-flex border-bottom border-end border-4 border-warning m-0 pe-3" style="min-width: 270px;">
+                <nav>
+                    <ul class="list-unstyled d-flex flex-column h-100 ps-2 pt-4">
+                        <a href="" class="text-decoration-none text-white">
+                            <li class="mb-5 bg-success rounded p-2">
+                                <a href="" class="text-decoration-none text-white">Itens para o casamento / Cálculo de Orçamentos</a>
+                            </li>
+                        </a>    
+
+                        <a href="" class="text-decoration-none text-white">
+                            <li class="mb-5 bg-success rounded p-2">
+                                Quadro de tarefas
+                            </li>
+                        </a>
+                        
+                        <a href="" class="text-decoration-none text-white">
+                            <li class="mb-5 bg-success rounded p-2">
+                                Procura de Locais
+                            </li>
+                        </a> 
+
+                        <a href="" class="text-decoration-none text-white">
+                            <li class="mb-5 bg-success rounded p-2">
+                                Detalhes do local
+                            </li>
+                        </a>
+                    </ul>
+                </nav>
+            </div>
+
+            <div class="d-flex w-100 justify-content-center align-items-center">
+                <h1 class="">Marry-Us</h1>
+            </div>
+        </div>
+    </Main>
+
+    <Footer>
+
+    </Footer>
+</body>
+</html>
