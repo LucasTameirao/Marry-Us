633413aa13cc4246776064708eb05179844dec91
Lucas Tameirao <lucas.atmartins@gmail.com>
2024-11-28 11:59:42 -0300

    qualquer coisa

--- html/process.php
+++ html/process.php
@@ -1,15 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-    <?php 
-        header("Location: functionPage.html");
-    ?>
+<?php 
+    include("../php/conexao.php");
 
-</body>
-</html>
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $stmt = $mysqli->prepare("SELECT email FROM email WHERE email = ?");
+    $stmt->execute([$email]);
+
+    $email = $stmt -> fetch();
+
+    if($email){
+        header("Location: functionPage.html");
+    }
+
+    
+    
+?>
+
